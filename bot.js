7c2cdb3d09a68e2e22c320314edabcf3b6ed2ab0
Luii <36324212+Gustavoluii@users.noreply.github.com>
2018-11-10 23:05:50 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,9 +45,10 @@ bot.on("message", async message => {
         
     }
     
-        if(cmd === `${prefix}a`){
+        if(cmd === `${prefix}msgsuporte`){
         if (message.member.hasPermission("ADMINISTRATOR")){
         message.channel.send(new Discord.RichEmbed().setColor(COR).setDescription("**Está com problemas ou dúvidas? clique na reação que deseja e aguarde o suporte.**\n\n<:Steve:510973155548856330> - Problemas com a sua conta.\n<:Boleto:510970296510054410> - Problemas com compras\n<:Cliente:510970368534773781> Solicitar TAG cliente.\n<:form:496404450286632960> Outras dúvidas.")).then(async msg => {
+            message.delete();
             const emoji = bot.emojis.find(c => c.name == "Steve");
             const emoji2 = bot.emojis.find(c => c.name == "Boleto");
             const emoji3 = bot.emojis.find(c => c.name == "Cliente");
